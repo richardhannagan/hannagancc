6850272b47d868a6ee0146b1da3c1e26cc70fea4
richardhannagan <3982808+richardhannagan@users.noreply.github.com>
2024-01-08 20:46:12 +1100

    to make mine

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,17 +1,21 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Richard Hannagan';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Developer Blog';
+  const blogFontHeadings = process.env.BLOG_FONT_HEADINGS
+    ? decodeURI(process.env.BLOG_FONT_HEADINGS)
+    : 'monospace';
+    const blogFontParagraphs = process.env.BLOG_FONT_PARAGRAPHS
+    ? decodeURI(process.env.BLOG_FONT_PARAGRAPHS)
+    : 'monospace';
 
   return {
     name,
     blogTitle,
-    footerText,
+    blogFontHeadings,
+    blogFontParagraphs
   };
 };
