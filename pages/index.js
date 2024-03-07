b118419d2099419c9d73c438b7a5bc22a6e9ab02
richardhannagan <3982808+richardhannagan@users.noreply.github.com>
2024-03-07 19:23:40 +1100

    favicon fix

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ posts, globalData }) {
   return (
     <Layout>
       <Head>
-        <link rel="icon" href="../public/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.png" type="image/x-icon" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
