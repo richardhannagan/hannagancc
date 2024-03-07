1e9dcd7514d7cf0d0642a025bbfa1818020ee3f6
richardhannagan <3982808+richardhannagan@users.noreply.github.com>
2024-03-07 18:44:21 +1100

    to update tests

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,6 +4,6 @@ describe('sample render test', () => {
   });
 
   it('displays the title text', () => {
-    cy.get('h1').contains('Developer Blog');
+    cy.get('h1').contains('developer blog');
   });
 });
