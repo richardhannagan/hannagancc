1b0f45fb3662c7ff8b6ebf7cd65bca838124298f
richardhannagan <3982808+richardhannagan@users.noreply.github.com>
2024-04-08 20:41:32 +1000

    good update well done

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,12 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="w-200 h-200 rounded-full block mx-auto overflow-hidden">
+        <Image src="/79850018.JPG" alt="me" width={250} height={250} />
+      </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           {name}
